1055a89396ce684d6d306e60798a6db61399112e
Matthew Morrone <matthewmorrone1@gmail.com>
2015-10-18 16:09:30 -0400

    just try and use the sidebar to send someone a message muahahahaha

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-
+console.clear()
 if (window.location.host === "www.google.com") {
 	$(".r").each(function() {
 		$(this).find("a").each(function() {
@@ -12,6 +12,49 @@ if (window.location.host === "www.facebook.com") {
 	if (!window.location.pathname.startsWith("/messages")) {
 		window.location.href = "https://www.facebook.com/messages";
 	}
+
+	var ul1 = $('ul[data-reactid*=".$ordered_list"]')
+	,	li1 = $('ul[data-reactid*=".$ordered_list"] li')
+	,	ul2 = $('ul[data-reactid*=".2:$more_online_friends"]')
+	,	li2 = $('ul[data-reactid*=".2:$more_online_friends"] li')
+	,	sep = $('._554m'); //.moreOnlineFriends
+
+	fbChatAugment()
+	setTimeout(function() {
+		fbChatAugment()
+	}, 2000)
+	setInterval(function() {
+		fbChatAugment()
+	}, 10000)
+
+	$(document).on("mouseover", ".fbChatOrderedList", function() {
+		fbChatAugment()
+
+		var $ul = $('ul[data-reactid*=".$ordered_list"]')
+		$('li', $ul).sort(function(a, b){
+			return (Math.round(Math.random()) - 0.5)
+		}).appendTo($ul)
+	})
+
+}
+function fbChatAugment() {
+	$('._554m').remove();
+	$('.moreOnlineFriends').remove();
+	$('ul[data-reactid*=".2:$more_online_friends"] li').each(function(index, value){
+		$('ul[data-reactid*=".$ordered_list"]').append(value);
+	});
+	$('ul[data-reactid*=".2:$more_online_friends"]').empty();
+	$("._42fz").each(function() {
+		if ($(this).find("._568-").text() !== "" || $(this).find(".sx_8ed4e7").length === 0) {
+			$(this).remove()
+		}
+	})
+	var $ul = $('ul[data-reactid*=".$ordered_list"]')
+	$('li', $ul).sort(function(a, b){
+		if($(a).find("._55lr span").text() < $(b).find("._55lr span").text()) return -1;
+		if($(a).find("._55lr span").text() > $(b).find("._55lr span").text()) return 1;
+		return 0;
+	}).appendTo($ul)
 }
 if (window.location.host === "inbox.google.com") {
 	$(".jS.kl").remove()
@@ -30,7 +73,7 @@ if (window.location.host === "gist.github.com") {
 }
 if (window.location.pathname === "/pin/create/extension/") {
 	$(".selected").click()
-	
+
 }
 var langs = ["javascript", "coffeescript", "css", "html", "livescript", "purescript", "autohotkey", "php", "json"];
 if (window.location.host.includes("github")) {
@@ -41,7 +84,7 @@ if (window.location.host.includes("github")) {
 		}
 		var lang = $(this).find(".repo-list-stats")[0].childNodes[0].textContent.trim().toLowerCase();
 		if ($.inArray(lang, langs) === -1) {
-			$(this).remove();	  
+			$(this).remove();
 		}
 	})
 }
