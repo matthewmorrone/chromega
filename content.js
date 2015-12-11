dc9bb2343958f6efb19a799709c6e3bb96b4057b
Matthew Morrone <matthewmorrone1@gmail.com>
2015-12-11 17:58:14 -0500

    added rudimentary adblock capability and fixed bugs with combining cracked article pages

--- content.js
+++ content.js
@@ -4,38 +4,50 @@ if (chrome.downloads) {
 	}
 }
 chrome.storage.sync.get(function(items) {
-	if (window.location.host.includes("cracked") && window.location.pathname.includes("article")) {
-		var loc, next, sec
+	if (window.location.host.includes("cracked")) {
+		console.clear()
+		console.log("wtf")
+		var loc, next, sec, block = ".mainFrameModule.contentTopModule, .rightColumn, .headerWrapper, .mainAd, .socialShareModule, [target='_blank'] img, .recommendedForYourPleasureModule.genericLeftModule, #Comments, .FacebookLike, .trc_related_container.trc_spotlight_widget, .footer, .socialShareAfterContent, script, iframe, .trc_related_container.trc_spotlight_widget, #taboola-autosized-2r"
+		$(block).remove()
+		
+		if (window.location.pathname.includes("article")) {
+			loc = window.location.href
+			if (loc.split("_").last() === "p2.html") {
+				nex = loc.replace("_p2.html", ".html")
+				window.location.href = nex
+			}
 
-		loc = window.location.href
-		if (loc.split("_").last() === "p2.html") {
-			nex = loc.replace("_p2.html", ".html")
-			window.location.href = nex
-		}
+			nex = loc.split(".")
+			nex.pop()
+			nex = nex.join(".") + "_p2.html"
+			sec = document.createElement("div")
 
-		nex = loc.split(".")
-		nex.pop()
-		nex = nex.join(".") + "_p2.html"
-		sec = document.createElement("div")
+			$(".leftColumn").find(".PaginationContent").remove()
+			$(".leftColumn").after(sec)
 
-		$(".leftColumn").find(".PaginationContent").remove()
-		$(".leftColumn").after(sec)
-
-		$(sec).addClass(".leftColumn")
-		$(sec).load(nex+" .leftColumn", function() {
-			$(sec).find("img").each(function() {
-				console.log($(this).attr("data-img"))
-				$(this).attr("src", $(this).attr("data-img"))
-				console.log($(this).attr("src"), $(this).attr("data-img"))
+			$(sec).addClass(".leftColumn")
+			$(sec).load(nex+" .leftColumn", function() {
+				$(sec).find(block).remove()
+				$(sec).find("header, footer").remove()
+				$(sec).find("img").each(function() {
+					$(this).attr("src", $(this).attr("data-img"))
+				})
+				$(".dropShadowBottomCurved").removeClass("dropShadowBottomCurved")
+				
 			})
-		})
-
+			
+		}	
+		$(block).remove()
 	}
-
-
-
-
-
+	if (window.location.host.includes("peopleclick")) {
+		$(function() {
+			$(".x-table-layout input[type='text']").each(function(i, o) {
+				if (i < 1 || i > 5) {$(this).focus().val("0").blur(); return}
+				$(this).focus().val("7.5").blur()
+			})
+			$("#BR1_btnSubmit_btn").focus()
+		})
+	}
 	if (window.location.host === "www.google.com" && items.google) {
 		$(".r").each(function () {
 			$(this).find("a").each(function () {
