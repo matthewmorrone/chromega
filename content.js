cd1fe9c91df04ccdd7826679db13cbdc3bfc4f13
Matthew Morrone <matthewmorrone1@gmail.com>
2015-12-22 15:24:37 -0500

    enhancements for cracked and removal of google search result tracking

--- content.js
+++ content.js
@@ -6,60 +6,116 @@ if (chrome.downloads) {
 chrome.storage.sync.get(function(items) {
 	if (window.location.host.includes("cracked")) {
 		console.clear()
-		console.log("wtf")
 		var loc, next, sec, block = ".mainFrameModule.contentTopModule, .rightColumn, .headerWrapper, .mainAd, .socialShareModule, [target='_blank'] img, .recommendedForYourPleasureModule.genericLeftModule, #Comments, .FacebookLike, .trc_related_container.trc_spotlight_widget, .footer, .socialShareAfterContent, script, iframe, .trc_related_container.trc_spotlight_widget, #taboola-autosized-2r"
 		$(block).remove()
-		
-		if (window.location.pathname.includes("article")) {
+		if (window.location.pathname.includes("article") 
+		    || window.location.pathname.includes("blog")
+		    || window.location.pathname.includes("personal-experiences")) {
 			loc = window.location.href
-			if (loc.split("_").last() === "p2.html") {
-				nex = loc.replace("_p2.html", ".html")
-				window.location.href = nex
+			if (window.location.pathname.includes("blog")) {
+				nex = loc.slice(0, -1) + "_p2/"
 			}
-
-			nex = loc.split(".")
-			nex.pop()
-			nex = nex.join(".") + "_p2.html"
+			else {
+				
+				if (loc.split("_").last() === "p2.html") {
+					nex = loc.replace("_p2.html", ".html")
+					window.location.href = nex
+				}
+				nex = loc.split(".")
+				nex.pop()
+				nex = nex.join(".") + "_p2.html"				
+			}
 			sec = document.createElement("div")
-
 			$(".leftColumn").find(".PaginationContent").remove()
+			$(".columnistsModule.genericLeftModule").remove()
 			$(".leftColumn").after(sec)
-
 			$(sec).addClass(".leftColumn")
-			$(sec).load(nex+" .leftColumn", function() {
+			$(sec).load(nex + " .leftColumn", function() {
 				$(sec).find(block).remove()
 				$(sec).find("header, footer").remove()
 				$(sec).find("img").each(function() {
 					$(this).attr("src", $(this).attr("data-img"))
 				})
 				$(".dropShadowBottomCurved").removeClass("dropShadowBottomCurved")
-				
 			})
-			
-		}	
+		}
+
 		$(block).remove()
 	}
+	if (window.location.host.includes("postmarkcu")) {
+		if (window.location.href.includes("home")) {
+			$("[name='userid']").val("matthewmorrone1")
+			$("[name='userid']").parents("tr").next().find("a[href*='submit'] img").click() //.css("-webkit-filter", "invert(1)")
+		}
+	}
+	if (window.location.host.includes("global1.onlinebank")) {
+		$("#accountListTable tr:last-child").remove()
+	}
 	if (window.location.host.includes("peopleclick")) {
+
 		$(function() {
-			$(".x-table-layout input[type='text']").each(function(i, o) {
-				if (i < 1 || i > 5) {$(this).focus().val("0").blur(); return}
-				$(this).focus().val("7.5").blur()
-			})
-			$("#BR1_btnSubmit_btn").focus()
+			setTimeout(function() {
+				$(".x-table-layout input[type='text']").each(function(i, o) {
+					if (i < 1 || i > 5) {
+						$(this).focus().val("0").blur();
+						return
+					}
+					$(this).focus().val("7.5").blur()
+				})
+				$("#BR1_btnSubmit_btn").focus()
+			}, 1000)
 		})
 	}
 	if (window.location.host === "www.google.com" && items.google) {
-		$(".r").each(function () {
-			$(this).find("a").each(function () {
-				console.log($(this).attr("href"))
-				$(this).removeAttr("onmousedown")
+		var a, href, datahref, text
+		$(".r").each(function() {
+			$(this).find("a").each(function() {
+				href = $(this).attr("href")
+				text = $(this).text()
+				a = $("<a></a>")
+				a.href = href
+				a.innerHTML = text
+				a.target = "_blank"
+				$(this).after(a)
 			})
 		})
+		$(document).on("click", ".r a", function(e) {
+			e.preventDefault()
+			href = $(this).attr("href")
+			text = $(this).text()
+			datahref = $(this).attr("data-href")
+			if (datahref && !$(this).hasClass(".noredirect")) {
+				a = $("<a></a>")
+				a[0].href = datahref
+				a[0].innerHTML = text
+				a.attr("class", "noredirect")
+				a[0].target = "_blank"
+				$(this).parent().after(a)
+				$(this).remove()
+				$(a).click()
+			}
+			// if ($(this).hasClass(".noredirect")) {
+			// 	$(this).off("click")
+			// 	$(this).click()
+			// }
+		})
+		// $(document).on("mouseover", ".noredirect", function(e) {
+		// 	e.preventDefault()
+		// 	// var win = window.open(href, '_blank');
+		// 	ev = $.Event('click')
+		// 	ev.which = 17
+		// 	ev.ctrlKey = true
+		// 	// ev.shiftKey = true
+		// 	console.log(ev, $(this), e)
+		// 	$(this).trigger(ev)
+		// 	// win.focus();				
+		// })
 	}
 	if (window.location.host === "inbox.google.com" && items.googleInbox) {
 		$(".jS.kl").remove()
+
 		function removeImages() {
-			setTimeout(function () {
+			setTimeout(function() {
 				$(".jS.kl").remove()
 				window.requestAnimationFrame(removeImages)
 			}, 1000)
@@ -67,28 +123,28 @@ chrome.storage.sync.get(function(items) {
 		// window.requestAnimationFrame(removeImages)
 	}
 	if (window.location.host === "gist.github.com" && items.githubGist) {
-		$('.js-quicksearch-field').on("focus", function () {
+		$('.js-quicksearch-field').on("focus", function() {
 			$(this).val("user:matthewmorrone1 ");
 		})
 	}
 	if (window.location.host.includes("github") && items.github) {
-		$('.js-site-search-focus').on("focus", function () {
-			$(this).val("user:matthewmorrone1 ");
-		})
-	// 	setTimeout(function() {
-	// 		$('.repo_filterer li:eq(2) a').click()
-	// 		$('.js-repo-filter-tab').eq(1).click()
-	// 		$("#js-pjax-container > div > div > div.column.three-fourths > div.tab-content.js-repo-filter > div > div > ul > li:nth-child(3) > a").click()
-	// }, 1000)
+		$('.js-site-search-focus').on("focus", function() {
+				$(this).val("user:matthewmorrone1 ");
+			})
+			// 	setTimeout(function() {
+			// 		$('.repo_filterer li:eq(2) a').click()
+			// 		$('.js-repo-filter-tab').eq(1).click()
+			// 		$("#js-pjax-container > div > div > div.column.three-fourths > div.tab-content.js-repo-filter > div > div > ul > li:nth-child(3) > a").click()
+			// }, 1000)
 	}
 	if (window.location.host.includes("pinterest") && items.pinterest) {
-		$(document).on("mouseover", ".item.selected", function (e) {
+		$(document).on("mouseover", ".item.selected", function(e) {
 			if ($(".pinnedToBoardWarning").length === 0) {
 				$(e.target).click()
 			}
 		})
 		if (window.location.pathname === "/pin/create/extension/") {
-			setTimeout(function () {
+			setTimeout(function() {
 				var $li = $("li").eq(0)
 				$li.find("button").show().click()
 				setTimeout(function() {
@@ -100,7 +156,3 @@ chrome.storage.sync.get(function(items) {
 });
 // github repo filter
 // https://gist.github.com/matthewmorrone1/60356a70f10619ced0bd
-
-
-
-
