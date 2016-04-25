a5893cc8430dee23612ea37f0d104bccd88b8033
Matthew Morrone <matthewmorrone1@gmail.com>
2016-04-25 11:11:07 -0400

    cleanup

--- content.js
+++ content.js
@@ -5,49 +5,7 @@ if (chrome.downloads) {
 }
 chrome.storage.sync.get(function(items) {
 
-(function($) {
-	$.fn.extend({
-		'tablesearcher': function(s) {
-			s = $.extend({}, $.fn.tablesearcher.defaults, s)
-			return this.each(function(i) {
-				var searchTimeout
-				var $table = $(this)
-				if (s.input == null) {
-					throw new Error('Must provide an input element to bind to')
-				}
-				s.input.bind('keyup', function() {
-					if (searchTimeout) {
-						clearTimeout(searchTimeout)
-					}
-					searchTimeout = setTimeout(initSearch, 300)
-				})
-				var lastQ = ''
-				function initSearch() {
-					var q = s.input.val().toLowerCase()
-					if (q == lastQ) return
-					if (q == '') {
-						$table.find('tbody tr').css('display', '')
-					} else {
-						//var tst = new RegExp(q, 'i')
-						var i = 0
-						$table.find('tbody tr').each(function() {
-							var $tr = $(this)
-							var searchStr = $tr.text().toLowerCase()
-								//var isMatch = tst.test(searchStr)
-							var isMatch = searchStr.indexOf(q) > -1
-							$tr.css('display', isMatch ? '' : 'none')
-						})
-					}
-					lastQ = q
-					searchTimeout = undefined
-				}
-			})
-		}
-	})
-	$.fn.tablesearcher.defaults = {
-		'input': null
-	}
-})(jQuery)
+
 
 	function sortHelper(c, d) {
 		return function(a, b) {
